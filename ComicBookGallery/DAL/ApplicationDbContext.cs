138a01de4c2084f497479c44018aaac058d914c7
thinh <thinhqn999@gmail.com>
2025-02-04 22:13:59 +0700

    Add project files.

---
+++ ComicBookGallery/DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ComicBookGallery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicBookGallery.DAL
+{
+  public class ApplicationDbContext:DbContext
+  {
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Books> Books { get; set; }
+  }
+}
---
+++ ComicBookGallery/Models/Books.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComicBookGallery.Models
+{
+  // code first
+  // đặt tên Bảng dữ liệu bằng DisplayColumn
+  //[DisplayColumn("Book")]
+  public class Books
+  {
+    //id tự động tăng
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    [Display(Name = "Tiêu đề cuốn sách")]
+    public string BookTitle { get; set; }
+    public string BookDescription { get; set; }
+    [Required]
+    public string Author { get; set; }
+
+
+  }
+}
