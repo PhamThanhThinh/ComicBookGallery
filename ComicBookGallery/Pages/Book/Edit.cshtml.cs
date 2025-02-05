0afbdd6432b313681abcc191c62768190b1b3284
thinh <thinhqn999@gmail.com>
2025-02-05 22:26:50 +0700

    final phần 2

--- ComicBookGallery/Pages/Book/Edit.cshtml.cs
+++ ComicBookGallery/Pages/Book/Edit.cshtml.cs
@@ -1,12 +1,50 @@
+using ComicBookGallery.DAL;
+using ComicBookGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComicBookGallery.Pages.Book
 {
-    public class EditModel : PageModel
+  public class EditModel : PageModel
+  {
+    private readonly ApplicationDbContext _context;
+
+    public EditModel(ApplicationDbContext context)
     {
-        public void OnGet()
-        {
-        }
+      _context = context;
     }
+
+    [BindProperty]
+    public Books Books { get; set; } = default;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+      if (id == null)
+      {
+        return RedirectToPage("./Index");
+      }
+
+      var book = await _context.Books.FirstOrDefaultAsync(b => b.Id == id);
+      if (book == null)
+      {
+        return RedirectToPage("./Index");
+      }
+      Books = book;
+      return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      _context.Update(Books);
+      await _context.SaveChangesAsync();
+      return Page();
+    }
+
+  }
 }
